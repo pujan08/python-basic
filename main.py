d4b101caf9ec23139d7d83bd9701f954ec51962c
pujan bhatt <65612957+pujan08@users.noreply.github.com>
2023-10-05 20:39:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-# Write your code below this line 👇
-print("My name is " + " " + "pujan")
-print("hey " + input("what is your name?"))
+print("welcome to the Band Name Generator")
+
+city = input("what's the name of city you grow up in?\n")
+
+pet = input("what's your pet name?\n")
+
+print("your band name could be "+city+" "+pet)
